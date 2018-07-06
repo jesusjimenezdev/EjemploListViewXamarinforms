56b284b9a0c09d7deaaf192a64f581f8fb465512
Jesus <jesus@iMac-de-Jesus.local>
2018-07-06 18:58:12 +0200

    final commit

---
+++ ListasDemo/ListasDemo/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ListasDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListasDemo.Helpers;
+
+namespace ListasDemo.ViewModel
+{    
+    public class MainPageViewModel
+    {
+        public ObservableCollection <Grouping<string, Friend>> Friends { get; set; }
+
+        public MainPageViewModel()
+        {
+            FriendRepository repository = new FriendRepository();
+            Friends = repository.GetAllGrouped();
+        }
+    }
+}
